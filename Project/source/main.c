c1a21e6a9ba9b6c9cc9fcc456cef4713ba1f9f33
Tharen Candi <tcan7909@uni.sydney.edu.au>
2023-01-13 11:00:31 +0100

    clean up

--- Project/source/main.c
+++ Project/source/main.c
@@ -8,10 +8,6 @@ int main(void) {
     //consoleDemoInit();
 	init_ships();
     configure_graphics();
-    new_shot_sprite(1, 3, 3);
-    new_shot_sprite(0, 5, 5);
-    set_target_coords(2,2);
-    player_target.is_hidden = 0;
 	// Initialize game FSM
 	GameState state = STATE_HOME; 
     while(1){
