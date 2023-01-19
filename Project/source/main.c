c975320771bc2b166f102fcce3eae9c459e7a449
Tharen Candi <tcan7909@uni.sydney.edu.au>
2023-01-19 17:23:52 +0100

    cleanup

--- Project/source/main.c
+++ Project/source/main.c
@@ -12,9 +12,6 @@ int main(void) {
 	configure_timers();
     configure_graphics();
     load_backgrounds(MAIN_MENU);
-	int tid = new_text("test text", 100,100, 0);
-	int i = 100;
-	int j = 100;
 #ifndef DEBUG
     wifi_init();
 #endif
@@ -26,15 +23,5 @@ int main(void) {
         swiWaitForVBlank();
         oamUpdate(&oamMain);
         oamUpdate(&oamSub);
-		update_text(tid, "test updated", i,j);
-		i += 8;
-		j += 8;
-		if (i > 150 || j > 150) {
-			i = 100;
-			j = 100;
-		}
-
-		for (int x = 0; x < 1000000; x ++);
-
     }
 }
